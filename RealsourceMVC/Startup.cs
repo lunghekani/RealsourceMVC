0eb9aff86a55fa6ba25df6eeea3df676aab052e9
lunghekani <lunghivlanga@gmail.com>
2021-07-19 11:01:49 +0200

    Add project files.

---
+++ RealsourceMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RealsourceMVC.Startup))]
+namespace RealsourceMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
