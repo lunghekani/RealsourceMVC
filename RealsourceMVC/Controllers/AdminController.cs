9b6813605186cb73cf72d7735a596fed14da3621
lunghekani <lunghivlanga@gmail.com>
2021-08-15 13:57:55 +0200

    admin screens

---
+++ RealsourceMVC/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealsourceMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Customers()
+        {
+            return View();
+        }
+
+        public ActionResult Orders()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Experts()
+        {
+            return View();
+        }
+    }
+}
